c3bd0ee99183199b87e24224bfcb9e6f929e100c
sachithanurosoft <sperera@nurosoft.id>
2022-09-09 07:44:56 +0530

    [UPT] - Cash limit exceed validation

--- models/account_account.py
+++ models/account_account.py
@@ -1,5 +1,5 @@
 from odoo import api, fields, models, _
-from odoo.exceptions import UserError
+from odoo.exceptions import UserError, ValidationError
 
 
 class AccountAccount(models.Model):
@@ -12,4 +12,33 @@ class AccountAccount(models.Model):
 class AccountPayment(models.Model):
     _inherit = "account.payment"
 
-    internal_purchase = fields.Boolean(string="Purchase Transfer Internal", defualt=False)
+    @api.model
+    def create(self, values):
+        if 'is_internal_transfer' in values:
+            if values['is_internal_transfer']:
+                amount = values['amount']
+                destination_journal = self.env['account.journal'].browse(values['destination_journal_id'])
+                default_account_id = destination_journal.default_account_id
+                if default_account_id.purchase_reconciliation:
+                    limit = default_account_id.cash_limit
+
+                    balances = {
+                        read['account_id'][0]: read['balance']
+                        for read in self.env['account.move.line'].read_group(
+                            domain=[('account_id', 'in', default_account_id.ids)],
+                            fields=['balance', 'account_id'],
+                            groupby=['account_id'],
+                        )
+                    }
+
+                    balance = balances.get(default_account_id.id, 0)
+                    if (balance + amount) > limit:
+                        maximum_allocation = limit - balance
+                        if maximum_allocation < 0:
+                            error = "Account Cash transfer limit already Exceeded"
+                            raise ValidationError(error)
+                        else:
+                            error = "You can only transfer LKR " + str(maximum_allocation) + \
+                                    " amount. Account transfer limit exceeded"
+                            raise ValidationError(error)
+        return super(AccountPayment, self).create(values)
